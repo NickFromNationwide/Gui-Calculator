8b161f19be9b401694279f78e1c0b951d9c1f93b
Nick <16737120+NickFromNationwide@users.noreply.github.com>
2020-03-09 17:46:31 -0700

    made code not suck

--- main.py
+++ main.py
@@ -18,15 +18,5 @@ class Main:
     elif self.op == '%':
       self.answer = self.num1 % self.num2
     else:
-      self.bad_op_responce = random.randint(1, 5)
-      if self.bad_op_responce == 1:
-        print "That operator was lost with the dinosoars."
-      if self.bad_op_responce == 2:
-        print "error.... error.... operator not found in memory banks. shutting down.......... \n \n *bzzzt*"
-      if self.bad_op_responce == 3:
-        print "Really? this is a CALCULATOR, not a AI! How is it expected to know every single operator in THE WHOLE FLIPPING WORLD?"
-      if self.bad_op_responce == 4:
-        print "That operator is only known to the Aztecs. Please consult them for the correct answer"
-      if self.bad_op_responce == 5:
-        print "Please do not use whatever alien operator you are using, and only use +, -, *, /, and %"
+      print("big stonks")
       print str(self.answer)
